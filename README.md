936aa12ff4fbea7bdac23a21e6e304910533011d
Rahul Ray <R4hulR@protonmail.com>
2023-12-30 14:49:27 +0530

    first and last

---
+++ README.md
@@ -0,0 +1,8 @@
+# Frontend Mentor - Expenses chart component
+
+![Design preview for the Expenses chart component coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+The data for the chart is fetched from the JSON File and chart is made using chart.js.
---
+++ app/app.js
@@ -0,0 +1,67 @@
+const Chart = window.Chart;
+const ctx = document.getElementById('canvas');
+
+fetch('../../data.json')
+  .then(response => response.json())
+  .then(data => {
+    createBarChart(data);
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+function createBarChart(data) {
+
+  const days = data.map(entry => entry.day);
+  const amounts = data.map(entry => entry.amount);
+
+  // Sum up the amounts
+  const totalAmount = amounts.reduce((acc, value) => acc + value, 0);
+  document.getElementById('total').textContent = '$'+`${totalAmount.toFixed(2)}`;
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: days,
+      datasets: [{
+        label: null, 
+        data: amounts,
+        backgroundColor: 'hsl(10, 79%, 65%)',
+        borderColor: 'hsl(10, 79%, 65%)',
+        borderWidth: 1,
+        borderSkipped:false,
+        borderRadius: 5,
+        hoverBackgroundColor:'hsl(186, 34%, 60%)',
+        hoverBorderColor: 'hsl(186, 34%, 60%)'
+      }]
+    },
+    options: {
+      scales: {
+        x: {
+          grid: {
+            display: false,
+            drawBorder:false
+          }
+        },
+        y: {
+          display:false,
+          grid: {
+            display: false 
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          display: false 
+        },
+        tooltip: {
+            mode: 'x', 
+            intersect: false,
+            callbacks: {
+                label: (context) => {
+                  return context.parsed.y;
+                }
+            }
+        }
+      }
+    }
+  });
+}
